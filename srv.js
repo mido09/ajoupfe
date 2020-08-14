03cbccdc7cfb65019185d3813b25a0bb80891a94
mido09 <eltaiefahmed2@gmail.com>
2020-08-14 02:55:19 +0200

    Removed serial port from server rest api

--- srv.js
+++ srv.js
@@ -34,7 +34,7 @@ res.end("OK");
 
 app.get("/setOff",(req,res)=>{
 console.log("Got request on /setOff");
-port.write('0');
+//port.write('0');
 var rss = {"message":"SetOFF Ok"};
 sensorStatus.state = "OFF";
 sensorStatus.alerting = "FALSE";
@@ -51,7 +51,7 @@ res.end(JSON.stringify(sensorStatus));
 
 app.get("/setOn",(req,res)=>{
 console.log("Got request on /setOn");
-port.write('1');
+//port.write('1');
 sensorStatus.state = "ON";
 io.emit("state","ON");
 var rss = {"message":"SetON Ok"};
@@ -60,7 +60,7 @@ res.end(JSON.stringify(sensorStatus));
 
 app.get("/reset",(req,res)=>{
 console.log("Got request on /reset");
-port.write('r');
+//port.write('r');
 var rss = {"message":"Reset Ok"};
 sensorStatus.alerting = "FALSE";
 io.emit("alerting","FALSE");
@@ -93,39 +93,39 @@ console.log("socket connected ...");
 
 });
 
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
+//const SerialPort = require('serialport')
+//const Readline = require('@serialport/parser-readline')
 
-const port = new SerialPort("/dev/ttyACM0", {baudRate:9600});
+//const port = new SerialPort("/dev/ttyACM0", {baudRate:9600});
 
-const parser = new Readline()
-port.pipe(parser)
+//const parser = new Readline()
+//port.pipe(parser)
 
 
-var lastLine = null;
+//var lastLine = null;
 
-parser.on('data', line => {
+//parser.on('data', line => {
 
 //console.log(`> ${line}`)
 
-var currentLine = parseFloat(line);
-if(lastLine!=null){
-var diff = currentLine - lastLine;
-if(Math.abs(diff)>5) 
-{
-var today = new Date();
-var date = today.getDate()+"/"+(today.getMonth()+1)+"/"+today.getFullYear();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-console.log(dateTime+" : Someone is moving");
-if(sensorStatus.state=="ON")
-{
-	sensorStatus.alerting = "TRUE";
-	io.emit("alerting","TRUE");
-}
-port.write('x');
-}
+//var currentLine = parseFloat(line);
+//if(lastLine!=null){
+//var diff = currentLine - lastLine;
+//if(Math.abs(diff)>5) 
+//{
+//var today = new Date();
+//var date = today.getDate()+"/"+(today.getMonth()+1)+"/"+today.getFullYear();
+//var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//var dateTime = date+' '+time;
+//console.log(dateTime+" : Someone is moving");
+//if(sensorStatus.state=="ON")
+//{
+//	sensorStatus.alerting = "TRUE";
+//	io.emit("alerting","TRUE");
+//}
+//port.write('x');
+//}
 //else console.log("No One is there");
-}
-lastLine = parseFloat(line);
-})
+//}
+//lastLine = parseFloat(line);
+//})
